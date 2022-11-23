1d7fc41f33465c6fd7360d2d8c3199766a751e5c
SawshaDev <contactcallimarie@gmail.com>
2022-11-23 12:13:07 -0800

    Setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+import re
+from pathlib import Path
+from setuptools import setup
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+path = Path(__file__).parent / "wharf" / "__init__.py"
+version = re.search(r'\d[.]\d[.]\d',path.read_text()).group(0) #type: ignore
+
+packages = [
+    'lanyard',
+]
+
+
+setup(
+    name='wharf',
+    author='SawshaDev',
+    version=version,
+    packages=packages,
+    license='MIT',
+    description='An minimal discord api wrapper that allows you to do what you want to do',
+    install_requires=requirements,
+    python_requires='>=3.8.0',
+)
--- wharf/__init__.py
+++ wharf/__init__.py
@@ -1,1 +1,10 @@
-__version__ = "1.0.0"
+__title__ = 'wharf'
+__authors__ = 'SawshaDev'
+__version__ = '1.0.0'
+__license__ = 'MIT'
+__copyright__ = 'Copyright (c) 2022 SawshaDev'
+
+from .client import *
+from .gateway import *
+from .http import *
+from .objects import *
