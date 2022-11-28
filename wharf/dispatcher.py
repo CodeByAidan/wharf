cdb62fc642ce02d27d91810d06046d6e9aa30070
SawshaDev <contactcallimarie@gmail.com>
2022-11-27 20:36:59 -0800

    New and improved dispatcher !!!

--- wharf/dispatcher.py
+++ wharf/dispatcher.py
@@ -7,6 +7,8 @@ from typing import TypeVar, Callable, Coroutine, Any
 import logging
 import asyncio
 
+from .objects import Message
+
 
 EventT = TypeVar("EventT")
 T = TypeVar("T")
@@ -20,8 +22,14 @@ class Dispatcher:
         self.events = {}
 
     def filter_events(self, event_type: EventT, event_data):
-        if event_type in ("message_create"):
+        if event_type in ("message_create", "message_update"):
+            if event_type == "message_update" and len(event_data) == 4:
+                return
+
+
             return Message(event_data)
+
+        
 
     def add_callback(self, event_name, func):
         if event_name not in self.events:
--- wharf/objects.py
+++ wharf/objects.py
@@ -24,4 +24,21 @@ class Guild:
         self.name = guild.get("name")
         self.id = guild.get("id")
         self.icon_hash = guild.get("icon")
+
+class User:
+    def __init__(self, data: dt.UserData):
+        self._from_data(data)
+
+    def _from_data(self, data):
+        self.name = data.get("username")
+        self.id = data.get("id")
         
+
+class Message:
+    def __init__(self, data: dt.MessageCreateData):
+        self._from_data(data)
+
+
+    def _from_data(self, message: dt.MessageData):
+        self.content = message.get("content")
+        self.author = User(message['author'])
